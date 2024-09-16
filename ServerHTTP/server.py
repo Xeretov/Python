44b673c58724e96f32c59c94db07375921b49d17
Xeretov <gioeleamendola@gmail.com>
2024-09-16 12:51:58 +0200

    update

--- ServerHTTP/server.py
+++ ServerHTTP/server.py
@@ -3,15 +3,15 @@ from flask import Flask, render_template, request
 api = Flask("__name__", template_folder="./templates")
 utenti = [["mariorossi@gamil.com","mariorossi85","baubau66",0]]
 
-@api.route('/', methods=['POST'])
+@api.route('/', methods=['GET'])
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
-@api.route('/regok', methods=['POST'])
+@api.route('/regok', methods=['GET'])
 def regok():
     return render_template("reg_ok.html")
 
-@api.route('/regko', methods=['POST'])
+@api.route('/regko', methods=['GET'])
 def regko():
     return render_template("reg_ko.html")
 
