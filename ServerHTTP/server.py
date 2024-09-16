5304795887f981238fd9268f199842496a69a8d3
Xeretov <gioeleamendola@gmail.com>
2024-09-16 12:10:08 +0200

    update

--- ServerHTTP/server.py
+++ ServerHTTP/server.py
@@ -1,7 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 api = Flask("__name__")
+utenti = [["mariorossi@gamil.com","mariorossi85","baubau66",0]]
+
 @api.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
+@api.route('/registrati', methods=['GET'])
+def registrati():
+    utente=[]
+    utente.append(request.args.get('email'))
+    utente.append(request.args.get('cf'))
+    utente.append(request.args.get('passw'))
+    utente.append(0)
+    if utente in utenti:
+        return render_template('reg_ok.html')
+    else:
+        return render_template('reg_ko.html')
 
 api.run(host="0.0.0.0", port=8085)
