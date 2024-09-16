e6b4493fe5ff30451fd5489ea4897c0a3c4333ea
Xeretov <gioeleamendola@gmail.com>
2024-09-16 11:59:43 +0200

    update

--- ServerHTTP/server.py
+++ ServerHTTP/server.py
@@ -1,10 +1,7 @@
 from flask import Flask, render_template
-
 api = Flask("__name__")
-
 @api.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
 api.run(host="0.0.0.0", port=8085)
-
