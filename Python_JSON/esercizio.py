897841abfa947d24c8338e95669ec841130fa423
Xeretov <gioeleamendola@gmail.com>
2024-09-18 12:54:31 +0200

    update

--- Python_JSON/esercizio.py
+++ Python_JSON/esercizio.py
@@ -1,4 +1,5 @@
 import json
+import jsonschema
 
 def JsonSerialize(data):
     with open("data_file.json", "w") as write_file:
@@ -8,11 +9,39 @@ def JsonDeserialize(sFile):
     with open(sFile, "r") as read_file:
         return json.load(read_file)
 
-def print_dictionary(dData):
-    for keys, values in dData.items():
-        print(keys,"\n",values)
+def print_dictionary(dData, deep=0):
+    for key, value in dData.items():
+        print("\t"*deep + key + ":", end="")
+        if isinstance(value, dict):
+            print("{")
+            print_dictionary(value, deep + 1)
+            print("\t"*deep + "}")
+        else:
+            print("{ " + str(value) + " }")
+
+schema = {
+    "type": "object",
+    "properties": {
+        "name": {"type": "string"},
+        "age": {"type": "number"},
+        "scores": {
+            "type": "array",
+            "items": {"type": "number"},
+        }
+    },
+    "required": ["name"],
+    "additionalProperties": False
+}
 
 
-sFilePath = "./Python_JSON/example_2.json"
+
+sFilePath = "./Python_JSON/example_3.json"
 data = JsonDeserialize(sFilePath)
+print()
 print_dictionary(data)
+try:
+    jsonschema.validate(data, schema)
+    print("\nL'istanza è coerente con lo schema")
+
+except jsonschema.exceptions.ValidationError:
+    print("\nL'istanza non è valida")
