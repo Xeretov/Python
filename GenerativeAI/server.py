b4a6b27b0fb0f05c487c5cd0b66902130ec679eb
Xeretov <gioeleamendola@gmail.com>
2024-10-30 16:48:17 +0100

    Update

--- GenerativeAI/server.py
+++ GenerativeAI/server.py
@@ -33,14 +33,17 @@ def generate():
         generated_text = result['candidates'][0]['content']['parts'][0]['text']
     except Exception:
         print(result)
-        safety = result['candidates'][0]['safetyRatings']
-        sexually_explicit = safety[0]['probability']
-        hate_speech = safety[1]['probability']
-        harassment = safety[2]['probability']
-        dangerous_content = safety[3]['probability']
-        dictionary = {'sexually_explicit':sexually_explicit,'hate_speech':hate_speech,'harassment':harassment,'dangerous_content':dangerous_content}
-        print(f"Uno di questi filtri ha probabilità troppo alte di attivamento:\n{dictionary}")
-        return f"Uno di questi filtri ha probabilità troppo alte di attivamento:\n{dictionary}"
+        if 'safetyRatings' in result['candidates'][0]:
+            safety = result['candidates'][0]['safetyRatings']
+            sexually_explicit = safety[0]['probability']
+            hate_speech = safety[1]['probability']
+            harassment = safety[2]['probability']
+            dangerous_content = safety[3]['probability']
+            dictionary = {'sexually_explicit':sexually_explicit,'hate_speech':hate_speech,'harassment':harassment,'dangerous_content':dangerous_content}
+            print(f"Uno di questi filtri ha probabilità troppo alte di attivamento:\n{dictionary}")
+            return f"Uno di questi filtri ha probabilità troppo alte di attivamento:\n{dictionary}"
+        else:
+            return "L'immagine caricata va contro le linee guida di Gemini."
     # Convert the markdown texto to html compatible
     html = convert_markdown_to_html(generated_text)
     return html
